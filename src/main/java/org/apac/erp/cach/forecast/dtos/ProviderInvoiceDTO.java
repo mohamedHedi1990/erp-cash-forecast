2e66db46bac2590144ed8c250deb3902c9e915d2
lobnaKh <lobna.khalsi2@gmail.com>
2020-07-02 00:41:27 +0200

    ajouter rectif

--- src/main/java/org/apac/erp/cach/forecast/dtos/ProviderInvoiceDTO.java
+++ src/main/java/org/apac/erp/cach/forecast/dtos/ProviderInvoiceDTO.java
@@ -2,6 +2,13 @@ package org.apac.erp.cach.forecast.dtos;
 
 import java.util.Date;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+import org.apac.erp.cach.forecast.enumeration.InvoiceStatus;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -13,9 +20,10 @@ public class ProviderInvoiceDTO {
 	private Integer invoiceNumber;
 
 	private Integer invoiceDeadlineInNumberOfDays;
-
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss", timezone = "Africa/Tunis")
 	private Date invoiceDeadlineDate;
 
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss", timezone = "Africa/Tunis")
 	private Date invoiceDate;
 
 	private Double invoiceTotalAmount;
@@ -28,8 +36,13 @@ public class ProviderInvoiceDTO {
 
 	private String providerLabel;
 
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss", timezone = "Africa/Tunis")
 	protected Date createdAt;
 
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss", timezone = "Africa/Tunis")
 	protected Date updatedAt;
+	
+	@Enumerated(EnumType.STRING)
+	private InvoiceStatus invoiceStatus;
 
 }
