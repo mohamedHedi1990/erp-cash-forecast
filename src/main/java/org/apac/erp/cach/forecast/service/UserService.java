c62681d258861780c441c836095a9fba2a401f2a
LamiaMessaoudi <Lamyyamessaoudi@gmail.com>
2020-12-19 12:28:02 +0100

    add security party

--- src/main/java/org/apac/erp/cach/forecast/service/UserService.java
+++ src/main/java/org/apac/erp/cach/forecast/service/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apac.erp.cach.forecast.persistence.entities.User;
 import org.apac.erp.cach.forecast.persistence.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +13,15 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepo;
+	@Autowired
+	PasswordEncoder encoder;
 
 	public List<User> findAllUsers() {
 		return userRepo.findAll();
 	}
 
 	public User saveNewUser(User user) {
+		user.setUserPassword(encoder.encode(user.getUserPassword()));
 		return userRepo.save(user);
 	}
 
