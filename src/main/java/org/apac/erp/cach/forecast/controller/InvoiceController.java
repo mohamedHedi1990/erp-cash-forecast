4169fcd639f619cd7dc444e536ad11c749db7ca1
mohamedHedi1990 <med.elhadi.boussada@supcom.tn>
2020-12-22 17:48:23 +0100

    modif

--- src/main/java/org/apac/erp/cach/forecast/controller/InvoiceController.java
+++ src/main/java/org/apac/erp/cach/forecast/controller/InvoiceController.java
@@ -1,7 +1,5 @@
 package org.apac.erp.cach.forecast.controller;
 
-import java.util.List;
-
 import org.apac.erp.cach.forecast.persistence.entities.Invoice;
 import org.apac.erp.cach.forecast.persistence.entities.PaymentRule;
 import org.apac.erp.cach.forecast.service.InvoiceService;
@@ -11,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +45,12 @@ public class InvoiceController {
 	@DeleteMapping("/{invoiceId}")
 	public void deleteInvoice(@PathVariable("invoiceId") Long invoiceId) {
 		 invoiceService.deleteInvoice(invoiceId);
+	}
+	
+	@CrossOrigin
+	@PutMapping("/{invoiceId}")
+	public void closeInvoice(@PathVariable("invoiceId") Long invoiceId) {
+		 invoiceService.closeInvoice(invoiceId);
 	}
 	
 	@CrossOrigin
