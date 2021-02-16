02d62118d6c0975519a7700b718625b254919a9b
mohamedHedi1990 <med.elhadi.boussada@supcom.tn>
2021-02-16 13:49:06 +0100

    modif

--- src/main/java/org/apac/erp/cach/forecast/constants/Utils.java
+++ src/main/java/org/apac/erp/cach/forecast/constants/Utils.java
@@ -2,6 +2,7 @@ package org.apac.erp.cach.forecast.constants;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
 import java.util.Locale;
 
 public class Utils {
@@ -28,13 +29,21 @@ public class Utils {
 	public static String convertAmountToStringWithSeperator(double amount1) {
 		DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
 		symbols.setGroupingSeparator('\'');
-		DecimalFormat df=new DecimalFormat("#,###.###", symbols);
-		String result=df.format(amount1);
-		System.out.println(result);
-		String initialAmount=result;
+		DecimalFormat df=new DecimalFormat("# ###.###", symbols);
+		NumberFormat numberFormat = NumberFormat.getInstance(java.util.Locale.FRENCH);
+		String initialAmount=numberFormat.format(amount1);
+		//String result=df.format(amount1);
+		
+		//String initialAmount=result;
+		
 
-		if (initialAmount.contains(".")) {
-			String [] tab = initialAmount.split("\\.");
+		if (initialAmount.contains(".") || initialAmount.contains(",")) {
+			String [] tab = null;
+			if(initialAmount.contains(".") ) {
+				tab = initialAmount.split("\\.");
+			} else {
+				tab = initialAmount.split(",");
+			}
 
 			if(tab[1].length() == 1) {
 				tab[1] = "." + tab[1] + "00";
