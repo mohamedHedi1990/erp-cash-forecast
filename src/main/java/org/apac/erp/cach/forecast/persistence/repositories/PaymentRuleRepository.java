c8bcf28b9744adb35c8a5e6428853955f1530bf3
Mohamed Ajel <ajelmohamed03@gmail.com>
2021-04-22 14:20:14 +0200

    Effect

--- src/main/java/org/apac/erp/cach/forecast/persistence/repositories/PaymentRuleRepository.java
+++ src/main/java/org/apac/erp/cach/forecast/persistence/repositories/PaymentRuleRepository.java
@@ -3,6 +3,7 @@ package org.apac.erp.cach.forecast.persistence.repositories;
 import java.util.Date;
 import java.util.List;
 
+import org.apac.erp.cach.forecast.enumeration.PaymentMethod;
 import org.apac.erp.cach.forecast.persistence.entities.BankAccount;
 import org.apac.erp.cach.forecast.persistence.entities.PaymentRule;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +14,5 @@ public interface PaymentRuleRepository extends JpaRepository<PaymentRule, Long> 
 List<PaymentRule> findByPaymentRuleAccountAndPaymentRuleDeadlineDateBetweenOrderByPaymentRuleDeadlineDateAsc(BankAccount bankAccount, Date startDate, Date endDate);
 List<PaymentRule> findByPaymentRuleAccountAndIsValidatedAndPaymentRuleDeadlineDateBeforeOrderByPaymentRuleDeadlineDateAsc(BankAccount bankAccount, boolean isvalidated, Date startDate);
 
+    List<PaymentRule> findBypaymentRulePaymentMethodIn(PaymentMethod[] paymentMethods);
 }
