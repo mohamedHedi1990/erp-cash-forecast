c5bd219ce0304717401320d16f88a85b187e58c2
ajelmohamed <42378394+ajelmohamed@users.noreply.github.com>
2021-06-17 20:02:24 +0200

    user management (#72)

--- src/main/java/org/apac/erp/cach/forecast/service/UserService.java
+++ src/main/java/org/apac/erp/cach/forecast/service/UserService.java
@@ -21,12 +21,32 @@ public class UserService {
 	}
 
 	public User saveNewUser(User user) {
-		user.setUserPassword(encoder.encode(user.getUserPassword()));
-		return userRepo.save(user);
+
+		if(user.getUserId() != null) {
+			User oldUser = userRepo.findOne(user.getUserId());
+			// utilisateur crée sans mot de passe
+			if(oldUser.getUserPassword()== null) {
+				user.setUserPassword(encoder.encode(user.getUserPassword()));
+			} else { // contexte de modifier le mot de passe
+				if(!user.getUserPassword().equals(oldUser.getUserPassword())) {
+					user.setUserPassword(encoder.encode(user.getUserPassword()));
+				}
+			}
+
+		} else {
+			user.setUserPassword(encoder.encode(user.getUserPassword()));
+		}
+
+		user = this.userRepo.save(user);
+
+		return user;
 	}
 
 	public User findUserById(Long userId) {
 		return userRepo.findOne(userId);
 	}
 
+	public void deleteById(Long userId) {
+		this.userRepo.delete(userId);
+	}
 }
